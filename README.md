2ed9a786fd83fa40378cae5c9446cd0d30c6d1eb
Jahmella Williams <simmons.jahmella@gmail.com>
2023-06-04 12:41:03 -0400

    Displays count

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 My counter app. My second application after a long sabbatical. At times I feel like I should be doing more, but I should have started with things like this from the beginning to save me from headaches. 
 
 Things to do:
-1. Display the count
-2. move the h1 and buttons to the middle of the screen
-3. style the buttons
+1. Display the count &#9989
+2. move the h1 and buttons to the middle of the screen &#9989
+3. style the buttons &#9989
--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 const countUp = document.getElementById('count-up');
 const countDown = document.getElementById('count-down');
 const reset = document.getElementById('reset')
+const displayCount = document.querySelector('#value')
 let count = 0;
 
 countUp.addEventListener('click', function(){
 
        count += 1;
        console.log(`Count is currently: ${count}`)
+       value.textContent = count;
 
 })
 
 countDown.addEventListener('click', function(){
     count -= 1;
     console.log(`Count is currently: ${count}`)
+    value.textContent = count;
 })
 
 reset.addEventListener('click', function(){
     count = 0;
     console.log(`Count is currently: ${count}`);
+    value.textContent = count;
 })
+
 
